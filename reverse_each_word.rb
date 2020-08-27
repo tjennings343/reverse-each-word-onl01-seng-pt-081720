99f788813018827c380539d638371c336deb22dd
Trey Jennings <tjennings343@gmail.com>
2020-08-27 04:56:39 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word(sentence1)
-  new_arr = sentence1.split(" ")
-  new_arr2 = []
-  new_arr.collect do |x|
-     new_arr2 << x.reverse
-  end
-  return new_arr2.join(" ")
+  sentence1.split.collect {|x| x.reverse}.join(" ")
 end
