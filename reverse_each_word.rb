0ad6c76af5a08fc1f2265c7b4026088ee482b35b
Trey Jennings <tjennings343@gmail.com>
2020-08-27 04:53:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ def reverse_each_word(sentence1)
   new_arr = sentence1.split(" ")
   new_arr2 = []
   new_arr.collect do |x|
-     new_arr2 << x.reverse
+     new_arr2.join("") << x.reverse
   end
-  return new_arr2.join(" ")
 end
